a23a09ddc8a2b0762a39fe1aece545fd2bc9c591
Nicholas Balfe <nickbalfe@icloud.com>
2019-11-22 03:17:25 -0500

    Update to game layer changes.

--- keyboards/exclusive/e6v2/oe/keymaps/nbalfe/keymap.c
+++ keyboards/exclusive/e6v2/oe/keymaps/nbalfe/keymap.c
@@ -5,8 +5,8 @@
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _BL  0
-#define _FL  1
-#define _GL  2 // Game layer that removes FL activation from spacebar. Some games need spacebar held!
+#define _GL  1 // Game layer that removes FL activation from spacebar. Some games need spacebar held!
+#define _FL  2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BL] = LAYOUT_hhkb(
@@ -16,18 +16,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_Z,    KC_X,    KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM,   KC_DOT,     KC_SLSH,    KC_RSFT,  KC_RSFT,
                 KC_LALT, KC_LGUI,               LT(_FL, KC_SPC),                               KC_RGUI, MO(_FL)
         ),
-    [_FL] = LAYOUT_hhkb(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,    KC_F12,  KC_DEL,
-        RESET,   _______, KC_UP,   _______, KC_VOLU, _______, _______, _______, _______, _______,  _______, _______,   _______, KC_SLEP,
-        _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, DF(_GL), _______, _______, _______, _______,    _______,   _______,      _______,
-        _______,       _______, _______, _______, KC_MUTE, DF(_BL), _______, _______, _______,   _______, _______, _______,    _______,
-                 _______, _______,               _______,                               _______,  _______
-        ),
     [_GL] = LAYOUT_hhkb(
         KC_ESC,  KC_1,    KC_2,    KC_3, KC_4, KC_5, KC_6, KC_7, KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,   KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC,  KC_BSLS,
         KC_LCTL, KC_A,    KC_S,    KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,     KC_QUOT,       KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM,   KC_DOT,     KC_SLSH,    KC_RSFT,  KC_RSFT,
                 KC_LALT, KC_LGUI,               KC_SPC,                               KC_RGUI, MO(_FL)
+        ),
+    [_FL] = LAYOUT_hhkb(
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,    KC_F12,  KC_DEL,
+        RESET,   _______, KC_UP,   _______, KC_VOLU, _______, _______, _______, _______, _______,  _______, _______,   _______, KC_SLEP,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, DF(_GL), _______, _______, _______, _______,    _______,   _______,      _______,
+        _______,       _______, _______, _______, KC_MUTE, DF(_BL), _______, _______, _______,   _______, _______, _______,    _______,
+                 _______, _______,               _______,                               _______,  _______
         ),
 };
